3d2ddb1ea38aca41b7924aed7faa5bab17739771
Ujesh Patel <pateluje@gmail.com>
2023-09-21 19:34:18 +0000

    Updated Position Test

--- src/test/java/com/PositionTest.java
+++ src/test/java/com/PositionTest.java
@@ -9,14 +9,10 @@ import com.levelup.forestsandmonsters.Position;
 
 public class PositionTest {
     @Test
-    public void initializationSetsXCoordinates () {
+    public void initializationPositionXYCoordinates  () {
         Position position = new Position(5, 5);
         assertEquals(5, position.coordinates.x);
-    }
-
-    @Test
-    public void initializationSetsYCoordinates () {
-        Position position = new Position(5, 5);
         assertEquals(5, position.coordinates.y);
     }
+
 }
