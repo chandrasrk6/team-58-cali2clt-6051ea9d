41249fb64883771f12002eb8b8773d67fe96e457
DIAMOND <kcgreetings@yahoo.com>
2023-09-21 19:01:03 +0000

    created new character oject and set string name, c

--- src/test/java/com/levelup/forestsandmonsters/features/Character.java
+++ src/test/java/com/levelup/forestsandmonsters/features/Character.java
@@ -1,14 +1,32 @@
 package com.levelup.forestsandmonsters.features;
 
+import com.levelup.forestsandmonsters.GameController;
+
 public class Character {
     /* variable declarations */
     String characterName = " C ";
+
+    Character testCharacter = new Character(characterName);
+    
 
     public String getCharacterName() {
         return characterName;
     }
 
     public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    @Override
+    public String toString() {
+        return "Character [characterName=" + characterName + "]";
+    }
+
+    public void setTestCharacter(Character testCharacter) {
+        this.testCharacter = testCharacter;
+    }
+
+    public Character(String characterName) {
         this.characterName = characterName;
     }
     
