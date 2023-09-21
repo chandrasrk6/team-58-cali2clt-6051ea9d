fe1670e66359b2f20ed708e441fe4447bc80c16e
DIAMOND <kcgreetings@yahoo.com>
2023-09-21 18:53:36 +0000

    created character class

---
+++ src/test/java/com/levelup/forestsandmonsters/features/Character.java
@@ -0,0 +1,16 @@
+package com.levelup.forestsandmonsters.features;
+
+public class Character {
+    /* variable declarations */
+    String characterName = " C ";
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+    
+
+}
