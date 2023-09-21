52ff4d4151cf4d92a32252504ceeaa02858a43c1
DIAMOND <kcgreetings@yahoo.com>
2023-09-21 19:44:13 +0000

    sync before

--- src/main/java/com/levelup/forestsandmonsters/Character.java
+++ src/main/java/com/levelup/forestsandmonsters/Character.java
@@ -1,31 +1,19 @@
 package com.levelup.forestsandmonsters;
-
-import java.beans.Transient;
-
 public class Character {
     /* variable declarations */
     String characterName = " C ";
 
-    Character testCharacter = new Character(characterName);
-    
-
     public String getCharacterName() {
         return characterName;
     }
-
-    public void setCharacterName(String characterName) {
+     public void setCharacterName(String characterName) {
         this.characterName = characterName;
     }
-
     @Override
     public String toString() {
         return "Please enter your Character Name:" + characterName + "]";
     }
-
-    public void setTestCharacter(Character testCharacter) {
-        this.testCharacter = testCharacter;
-    }
-
     public Character(String characterName) {
         this.characterName = characterName;
-    }}
+    }
+}
