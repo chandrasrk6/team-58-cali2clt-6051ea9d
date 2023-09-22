87392ceac401030871ae9095af6398839ee2f9f8
icspots <b.elrousan@gmail.com>
2023-09-22 13:44:02 +0000

    Hello

---
+++ src/test/java/com/levelup/forestsandmonsters/GameMapTest.java
@@ -0,0 +1,60 @@
+  package com.levelup.forestsandmonsters;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+import static com.levelup.forestsandmonsters.GameController.DIRECTION.*;
+
+import java.awt.Point;
+
+public class GameMap {
+    static final int DEFAULT_POSITIONS = 100;
+    static final Position DEFAULT_START_POSITION = new Position(0, 0);
+    int numPositions;
+
+    public GameMap() {
+        this.numPositions;
+
+
+        public Position calculatePosition(Position startingPosition, DIRECTION direction) {
+            Position newPosition = startingPosition;
+
+            if (startingPosition == null) {
+                newPosition = DEFAULT_START_POSITION;
+            }
+
+            if (direction == NORTH) {
+                newPosition = new Position(newPosition.getX(), newPosition.getY() + 1);
+
+            }else if (direction == SOUTH) {
+                newPosition = new Position(newPosition.getX(), newPosition.getY() - 1);
+
+            }else if (direction == EAST) {
+                newPosition = new Position(newPosition.getX() + 1, newPosition.getY());
+
+            }else if (direction == WEST) {
+                newPosition = new Position(newPosition.getX() - 1, newPosition.getY());
+            }
+
+            if (isPositionValid(newPosition.coordinates) == true) {
+                return newPosition;
+            } else {
+                return startingPosition;
+
+            }
+            }
+    
+
+
+
+        
+
+        
+
+            }
+            }
+            })
+        }
+    }
+}
+public class GameMap {
+    
+} 
